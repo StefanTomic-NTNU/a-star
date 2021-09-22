2854a82d91c95b1649d93b5dd16c0eb04b1efe9e
pathel <polegend@stud.ntnu.no>
2021-09-22 15:43:21 +0200

    created searchNode class

---
+++ SearchNode.py
@@ -0,0 +1,14 @@
+
+
+class SearchNode():
+
+
+
+    def __init__(self, state, g, h, status, parent, kids):
+        self.state = state #object state check States.py
+        self.h = h
+        self.g = g
+        self.f = g + h # estimated total cost of a solution path
+        self.parent = parent #pointer to best parent node
+        self.status = status #string "OPEN" or "CLOSE"
+        self.kids = kids #a list maybe
