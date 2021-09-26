be2f5c85ff0f83a401191d25d96e2acffbcb98d9
Stefan Djordje Tomic <stefandt@stud.ntnu.no>
2021-09-26 16:04:16 +0200

    FEAT: task 4 solved (#4)

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ if __name__ == '__main__':
     mp1 = Map_Obj(1, resources_path=csv_dir)
     search1 = Search(mp1, root_filepath, animate=True)
     best_path = search1.find_best_path()
-    print(best_path)
 
     # Task 2
     mp2 = Map_Obj(2, resources_path=csv_dir)
@@ -22,3 +21,8 @@ if __name__ == '__main__':
     mp3 = Map_Obj(3, resources_path=csv_dir)
     search3 = Search(mp3, root_filepath, animate=True)
     best_path3 = search3.find_best_path()
+
+    # Task 4
+    mp4 = Map_Obj(4, resources_path=csv_dir)
+    search4 = Search(mp4, root_filepath, animate=True)
+    best_path4 = search4.find_best_path()
